cf71b56958d9e58ba4086ad84e41ac88937602b3
MLB-78 <100378645+MLB-78@users.noreply.github.com>
2022-02-27 18:07:43 +0100

    Les Booléens

---
+++ Les Booléens.js
@@ -0,0 +1,37 @@
+// les booléens
+// C'est un type de donné qui peut prendre uniquement deux valeurs : (true et false)
+
+// L'opérateur strictement supérieur
+console.log(100 > 70);
+// L'opérateur strictement inférieur
+console.log(70 < 100);
+// L'opérateur  supérieur ou égal 
+console.log(70 >= 70);
+// L'opérateur inférieur ou égal
+console.log(70 <= 70);
+// L'opérateur égalité stricte
+console.log(5 === 5);
+// L'opérateur inégalité stricte 
+console.log(7 !== 9)
+
+// LES COMBINAISONS DE COMPARAISONS 
+
+// L'opérateur && (ET) 
+
+// Cet opérateur est utilisé avec des valeurs non-booléennes, il renverra une valeur non-booléenne : ici 68
+// (C'est toujours la plus petite) 
+console.log(70 && 68);
+
+// Quand c'est utilisé avec des valeurs booléennes, il renvoie une valeur booléenne
+console.log(5 > 0 && 10 > 5);
+
+
+// L'opérateur || (OU)
+
+// L'opérateur OU va renvoyer vrai dans la console si l'une des deux opérandes est vrai
+console.log(70 > 0 || 50 < 70);
+
+
+
+// L'opérateur ! (NON) c'est de la merde
+
