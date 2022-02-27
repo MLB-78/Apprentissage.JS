c0dd8b01330b0e162cdaf4d9baa2490b25d583d6
MLB-78 <100378645+MLB-78@users.noreply.github.com>
2022-02-27 16:42:53 +0100

    Add files via upload

--- Les chaînes de caractères.js
+++ Les chaînes de caractères.js
@@ -1,3 +1,8 @@
 // les chaînes de caractères
-// console.log("Ceci est une chaîne de caractère");
-// console.log("C'est la première" + " chose que j'ai appris");
+console.log("Ceci est une chaîne de caractère");
+// Ceci est une concaténation de chaînes de caractères
+console.log("C'est la première" + " chose que j'ai appris");
+// Ceci est toujours une concaténation de chaînes de caractères
+console.log("1" + "8");
+
+
