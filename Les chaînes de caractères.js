c09f02e3c17c58abaae0e75571d877253a3894a2
MLB-78 <100378645+MLB-78@users.noreply.github.com>
2022-02-27 17:23:29 +0100

    Les chaînes de caractères

--- Les chaînes de caractères.js
+++ Les chaînes de caractères.js
@@ -3,6 +3,6 @@ console.log("Ceci est une chaîne de caractère");
 // Ceci est une concaténation de chaînes de caractères
 console.log("C'est la première" + " chose que j'ai appris");
 // Ceci est toujours une concaténation de chaînes de caractères
-console.log("1" + "8");
+console.log("1" + "8"); 
 
 
