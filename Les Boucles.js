d6f9512249ae66224b2b35dbd35f32f63d7c5edf
MLB-78 <100378645+MLB-78@users.noreply.github.com>
2022-02-27 23:35:16 +0100

    Les Boucles

---
+++ Les Boucles.js
@@ -0,0 +1,17 @@
+// Les Boucles 
+
+let a = 0;
+
+while (a < 2) {
+    console.log("On t'a dit")
+    a = a + 1;
+}
+
+// Quand tu sais pas 
+
+let njo;
+
+do {
+    njo = njo +2;
+    console.log("OATS")
+} while (njo < 8);
