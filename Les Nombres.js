23c703b885a6ebfdfdd942d48ad336e0f2130b62
MLB-78 <100378645+MLB-78@users.noreply.github.com>
2022-02-27 17:22:38 +0100

    Les nombres

---
+++ Les Nombres.js
@@ -0,0 +1,8 @@
+// les nombres
+
+// Ceci est une multiplication
+console.log(3 * 3);
+// Ceci est une addition
+console.log(7 + 7);
+// Ceci est calcul fesant appelle à plusieur opérations
+console.log(7*10 / 2 + 2)
