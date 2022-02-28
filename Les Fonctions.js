5c80ffbd26864bbe4c9c476b7f28c7d69975fb4f
MLB-78 <100378645+MLB-78@users.noreply.github.com>
2022-02-28 14:12:44 +0100

    Les Fonctions

---
+++ Les Fonctions.js
@@ -0,0 +1,47 @@
+// les fonctions
+
+// Déclaration de la fonction : presenteToi - Ce qui se situe entre parenthèse c'est les paramètres
+function presenteToi(prenom,nom,age,taille,poids) {
+    console.log(prenom +" " + nom);
+    console.log(age + " ans");
+    console.log("Mensuration : " + taille + " " + poids);
+}
+
+
+// Appel de la fonction : presenteToi - Ici ce qui se situe entre parenthèse c'est les arguments 
+presenteToi("Maxime","Njo Bojongo",18,"1m 85","83 kg");
+    
+
+
+// RETURN 
+// Le mot clé "return" permet de retourner une valeur - Attention return doit toujours se situer à la fin de la fonction
+// return se comporte vraiment comme une variable sans le égal 
+// Je considere personnelement return comme un "Scope Locale" car elle toujours être utilisé dans une fonction
+
+// AVEC VARIABLE
+
+let som;
+
+// déclaration
+function multiplication(operande1,operande2) {
+    return operande1*operande2;
+}
+
+// l'appel de la fonction est situé dans la variable
+som = multiplication(5,10);
+
+console.log(som);
+
+
+
+// SANS VARIABLE
+
+// déclaration
+function soustraction(x,y) {
+    return x-y;
+}
+
+// l'appel est situé dans l'instruction console.log
+console.log(soustraction(10,8));
+console.log(soustraction(10,10));
+
