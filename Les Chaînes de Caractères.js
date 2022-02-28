12493d14112f2f82a36a914324f76a66b4906d59
MLB-78 <100378645+MLB-78@users.noreply.github.com>
2022-02-28 15:43:27 +0100

    Les Chaînes de caractères

--- Les Chaînes de Caractères.js
+++ Les Chaînes de Caractères.js
@@ -5,5 +5,6 @@ console.log("C'est la première" + " chose que j'ai appris");
 // Ceci est toujours une concaténation de chaînes de caractères
 console.log("1" + "8"); 
 
-
+// Pour faire un break(un saut de ligne) dans l'instruction on fait \n
+console.log("Ippo, \nSawamura Ryohei,\nRandy Boy JR");
  
