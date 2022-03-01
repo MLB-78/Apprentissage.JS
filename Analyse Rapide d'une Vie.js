46093ea3da8dee74108f1659440d60f2c161c66a
MLB <100378645+MLB-78@users.noreply.github.com>
2022-03-01 17:00:35 +0100

    Analyse Rapide d'une Vie
    J'ai échoué sur un exercice où il fallait indiqué des heures en JS ( les boucles for me zaef ) et je me suis rabattu sur un truc super facile à réaliser sans aucune intérêt 
    BREF
    La route est longue pour être dév

---
+++ Analyse Rapide d'une Vie.js
@@ -0,0 +1,42 @@
+// Ta journée random d'un éléve de merde 
+
+// à quel heure tu te lève
+let reveille = 11.30;
+// à quel heure tu commence à coder
+let tuCodeQuand = 14;
+// de 0 à 10 est ce que le mental est toujoours la
+let mental = 7;
+// combien de temps tu perds dans la journée
+let tempsPerdu = 16;
+// ou en est ta vie sociale sur une échelle de 0 à 10
+let vieSociale = 6.5;
+// on en est ta vie amoureuse sur une échelle de 0 à 10 
+let amour = 0;
+
+
+
+habitude = () => {
+    console.log( "Maxime tu te lève à " + reveille);
+    console.log( "Maxime tu code  à " + tuCodeQuand);
+    console.log( "Maxime ton mental est de " + mental);
+    console.log( "Maxime sur 24h dans une journée tu en perd  " + tempsPerdu);
+    console.log( "Maxime ta vie sociale est estimé à  " + vieSociale);
+    console.log( "Maxime t'as vie amoureuse est à "+ amour + ". Elle n'a juste pas encore commencé");
+    console.log("mais on est toujours en vie alors ");
+
+    if (mental > vieSociale) {
+        console.log("alors on fait avec :)");
+        
+    }
+}
+
+habitude();
+
+
+
+// Je suis fâcher avec les boucles en Javascript
+
+
+
+
+
