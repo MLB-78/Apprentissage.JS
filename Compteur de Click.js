0dab5128bcd315e84794f3f4e6afd3feb03c326e
MLB <100378645+MLB-78@users.noreply.github.com>
2022-03-01 23:20:48 +0100

    Compteur de Click

---
+++ Compteur de Click.js
@@ -0,0 +1,11 @@
+// Créer un compteur de clik sur une page web
+
+compteurDeClik = () => {
+    // Le scope ici est locale mais il peut très bien être locale
+    let clik = 0;
+    // L'opérateur d'incrémentation ++
+     return clik++ ;
+
+}
+
+{/* <h1 onclick="console.log(compteurDeClik())"> igamu</h1>  */}
