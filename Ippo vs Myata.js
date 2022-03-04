7ebd9f0e7a12a9b22aea44133c8bf66a2da67368
MLB <100378645+MLB-78@users.noreply.github.com>
2022-03-04 14:21:34 +0100

    Qui est le plus fort

---
+++ Ippo vs Myata.js
@@ -0,0 +1,45 @@
+// Savoir qui est le équilibré entre Ippo et Myata
+// Sur 10
+
+fight = () => {
+
+    
+    let ippoPuissance = 8.5;
+    let ippoRapidité = 7.5;
+    let ippoTalent = 6.5
+
+
+
+    let myataPuissance = 6.5;
+    let myataRapidité = 9;
+    let myataTalent = 8;
+
+   if (ippoPuissance > myataPuissance) {
+       console.log("Ippo est plus puissant que Ichirô");
+   } else {
+       console.log("Myata est plus fort que ippo");
+   }
+
+   if (ippoRapidité > myataRapidité) {
+       console.log("Ippo est plus rapide que Ichirô");
+   } else {
+       console.log("Myata est plus rapide que ippo");
+   }
+
+   if (ippoTalent > myataTalent) {
+       console.log("Ippo est plus talentueux que Ichirô");
+   } else {
+       console.log("Myata est plus talentueux que ippo");
+
+   }
+
+   const conclusion = "Myata est plus complet que ippo"
+
+
+
+
+
+
+}
+
+fight();
