1ae8fd79f235a401e78408b7b7e789a8398a7d4b
MLB <100378645+MLB-78@users.noreply.github.com>
2022-03-06 12:50:38 +0100

    L'opérateur Typeof

---
+++ Typeof.js
@@ -0,0 +1,15 @@
+// L'opérateur typeof
+// L'opérateur typeof renvoie une chaîne qui indique le type de son opérande.
+
+
+let oats = "ziak";
+let poids = 84;
+let quiEstQui = 17 > 18;
+let rr;
+
+console.log(typeof oats);
+console.log(typeof poids);
+console.log(typeof quiEstQui);
+console.log(typeof rr);
+
+
