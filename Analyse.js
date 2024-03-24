a1be862d112e6732cf5d75e9e16cc2c4b57837f3
Maxime <100378645+MLB-78@users.noreply.github.com>
2024-03-24 16:57:28 +0100

    Update and rename Analyse Rapide d'une Vie.js to Analyse.js

---
+++ Analyse.js
@@ -0,0 +1,34 @@
+
+let reveille = 11.30;
+let tuCodeQuand = 14;
+let mental = 7;
+let tempsPerdu = 16;
+let vieSociale = 6.5;
+let amour = 0;
+
+
+
+habitude = () => {
+    console.log( "Ndoki tu te lève à " + reveille);
+    console.log( "Ndoki tu code  à " + tuCodeQuand);
+    console.log( "Ndoki ton mental est de " + mental);
+    console.log( "Ndoki sur 24h dans une journée tu en perd  " + tempsPerdu);
+    console.log( "Ndoki ta vie sociale est estimé à  " + vieSociale);
+    console.log("mais on est toujours en vie alors ");
+
+    if (mental > vieSociale) {
+        console.log("alors on fait avec :)");
+        
+    }
+}
+
+habitude();
+
+
+
+// Je suis fâcher avec les boucles en Javascript
+
+
+
+
+
